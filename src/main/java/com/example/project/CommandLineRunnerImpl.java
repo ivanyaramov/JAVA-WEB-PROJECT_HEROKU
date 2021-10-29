2b65a98a751763bf091ba04b513b5422d837b0ae
ivan <ivan.yaramov@gmail.com>
2021-10-29 11:30:13 +0300

    testcommit

--- src/main/java/com/example/project/CommandLineRunnerImpl.java
+++ src/main/java/com/example/project/CommandLineRunnerImpl.java
@@ -5,6 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 public class CommandLineRunnerImpl implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println(5);
     }
 }
