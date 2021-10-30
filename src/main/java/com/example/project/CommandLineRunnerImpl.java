589d5e54059ceb913fc3fdb979f0890c113b3d83
ivan <ivan.yaramov@gmail.com>
2021-10-30 16:43:03 +0300

    addinghotels

--- src/main/java/com/example/project/CommandLineRunnerImpl.java
+++ src/main/java/com/example/project/CommandLineRunnerImpl.java
@@ -1,6 +1,7 @@
 package com.example.project;
 
 import com.example.project.service.CountryService;
+import com.example.project.service.HotelService;
 import com.example.project.service.TownService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -9,15 +10,18 @@ import org.springframework.context.annotation.Configuration;
 public class CommandLineRunnerImpl implements CommandLineRunner {
     private final CountryService countryService;
     private final TownService townService;
+    private final HotelService hotelService;
 
-    public CommandLineRunnerImpl(CountryService countryService, TownService townService) {
+    public CommandLineRunnerImpl(CountryService countryService, TownService townService, HotelService hotelService) {
         this.countryService = countryService;
         this.townService = townService;
+        this.hotelService = hotelService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         countryService.initaliseCountries();
         townService.initialiseTowns();
+        hotelService.initialiseHotels();
     }
 }
--- src/main/java/com/example/project/repository/CountryRepository.java
+++ src/main/java/com/example/project/repository/CountryRepository.java
@@ -2,6 +2,7 @@ package com.example.project.repository;
 
 import com.example.project.model.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
